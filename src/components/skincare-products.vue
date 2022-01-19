4ba9c3b6132f3cffb0dfaaefe84d227b9999344a
harihaysrun <nursyahirah98@outlook.com>
2022-01-19 09:09:48 +0000

    linked api to show list of skincare products

--- src/components/skincare-products.vue
+++ src/components/skincare-products.vue
@@ -5,15 +5,30 @@
       <h1>View All Skincare Products</h1>
       <button><a href="/skincare-products/add">Add new product</a></button>
 
+      <ol>
+        <li v-for="p in products" v-bind:key="p._id">
+          <b>{{p.productBrand}}</b> {{p.productName}}
+        </li>
+      </ol>
+
     </div>
     
 </template>
 
 <script>
+import axios from 'axios';
+const BASE_API_URL = "https://3000-harihaysrun-skincareapi-fj1wbad540x.ws-us27.gitpod.io/";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  created: async function(){
+    let response = await axios.get(BASE_API_URL + 'skincare-products');
+    this.products = response.data;
+    console.log(this.products)
+  },
+  data: function(){
+    return{
+      'products': []
+    }
   }
 }
 </script>
