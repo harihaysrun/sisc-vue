daedacc7c6703282a65e062db4ebd133f4235db9
harihaysrun <nursyahirah98@outlook.com>
2022-01-26 10:37:29 +0000

    need to figure out how to do search filter without asking user to tick in everything

--- src/components/products-list.vue
+++ src/components/products-list.vue
@@ -7,16 +7,60 @@
 
       <div v-bind:style="{'display': display}" style="background-color:rgba(0,0,0,0.1); padding: 20px;">
         <h2 style="margin:0;">Search:</h2>
+
         <input type="text" v-model="search" placeholder="Search by brand or product name">
-        <p>{{search}}</p>
+
+        <div>
+          <label>Product condition</label>
+          <input type="radio" value="New" v-model="product_condition" /> Brand New
+          <input type="radio" value="Used" v-model="product_condition" /> Used
+        </div>
+
+        <div>
+          <label>Product Category</label>
+          <select name="" id="" v-model="product_category">
+            <option value="" disabled>select one</option>
+            <option value="First Cleanser">First Cleanser</option>
+            <option value="Second Cleanser">Second Cleanser</option>
+            <option value="Toner">Toner</option>
+            <option value="Serum">Serum</option>
+            <option value="Essence">Essence</option>
+            <option value="Ampoule">Ampoule</option>
+            <option value="Treatment">Treatment</option>
+            <option value="Moisturiser">Moisturiser</option>
+            <option value="Treatment">Treatment</option>
+            <option value="Sheet Mask">Sheet Mask</option>
+            <option value="Clay Mask">Clay Mask</option>
+            <option value="Chemical Exfoliant">Chemical Exfoliant</option>
+            <option value="Physical Exfoliant">Physical Exfoliant</option>
+            <option value="Eye Cream">Eye Cream</option>
+            <option value="Body">Body</option>
+            <option value="Others">Others</option>
+          </select>
+
+        </div>
+
+        <p>
+          {{search}}
+          {{product_condition}}
+          {{product_category}}
+          <br>
+        </p>
       </div>
 
       <ol>
         <li v-for="p in filteredProducts.slice(0,max)" v-bind:key="p._id">
           <a v-on:click="viewThisProduct(p._id)" style="display:flex; flex-direction:row; align-items:center">
             <img style="width:150px; display:block" v-bind:src="p.productImage" />
-            <b>{{p.productBrand}}</b> {{p.productName}}
-            {{p._id}}
+            {{p.productBrand}}
+            <br>
+            {{p.productName}}
+            <br>
+            {{p.productCondition}}
+            <br>
+            {{p.productCategory}} {{p.productCategoryOthers}}
+            <!-- <br>
+            {{p._id}} -->
           </a>
         </li>
       </ol>
@@ -39,7 +83,9 @@ export default {
   data: function(){
     return{
       'products': [],
-      'search': ''
+      'search': '',
+      'product_condition': '',
+      'product_category': '',
     }
   },
   props: ['pageTitle', 'max', 'display'],
@@ -53,10 +99,12 @@ export default {
   computed:{
     filteredProducts: function(){
       let filtered = this.products.filter((p) =>
-        p.productBrand.toLowerCase().includes(this.search.toLowerCase()) ||
-        p.productName.toLowerCase().includes(this.search.toLowerCase())
-        );
-        return filtered;
+        // p.productBrand.toLowerCase().includes(this.search.toLowerCase()) ||
+        // p.productName.toLowerCase().includes(this.search.toLowerCase()) ||
+        p.productCondition === this.product_condition ||
+        p.productCategory === this.product_category
+      );
+      return filtered;
     }
   }
 }
