84d967220c5e74dded810521f9c3ce1222e84875
harihaysrun <nursyahirah98@outlook.com>
2022-02-07 15:43:05 +0000

    fixed the [OP] appearing when no name is added in the field

--- src/components/product-info.vue
+++ src/components/product-info.vue
@@ -117,7 +117,8 @@
             <div class="each-comment" v-for="c in comments" v-bind:key="c._id">
 
               <div>
-                <b class="c-name" v-if="c.commentName.toLowerCase() === poster_name.toLowerCase() || poster_name.toLowerCase().includes(c.commentName.toLowerCase())">[OP] {{c.commentName}}</b>
+                <b class="c-name" v-if="!c.commentName">{{c.commentName}}</b>
+                <b class="c-name" v-else-if="c.commentName.toLowerCase() === poster_name.toLowerCase() || poster_name.toLowerCase().includes(c.commentName.toLowerCase())">[OP] {{c.commentName}}</b>
                 <b class="c-name" v-else>{{c.commentName}}</b>
               </div>
 
--- src/components/request-info.vue
+++ src/components/request-info.vue
@@ -86,7 +86,8 @@
           <div class="each-comment" v-for="c in comments" v-bind:key="c._id">
             
             <div class="offer">
-              <b class="c-name" v-if="c.commentName.toLowerCase() === poster_name.toLowerCase() || poster_name.toLowerCase().includes(c.commentName.toLowerCase())">[OP] {{c.commentName}}</b>
+              <b class="c-name" v-if="!c.commentName">{{c.commentName}}</b>
+              <b class="c-name" v-else-if="c.commentName.toLowerCase() === poster_name.toLowerCase() || poster_name.toLowerCase().includes(c.commentName.toLowerCase())">[OP] {{c.commentName}}</b>
               <b class="c-name" v-else>{{c.commentName}}</b>
               <div v-if="c.commentOffer">
                 <span class="details-tag">offer:</span>
